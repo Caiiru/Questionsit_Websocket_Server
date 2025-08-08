739b868c678950c5bcb0f8ccf86722c249b2b2fb
Caiiru <54562856+Caiiru@users.noreply.github.com>
2025-08-08 18:44:04 -0300

    Refactor: Clen quiz controller

--- src/Quiz/controller/quizController.ts
+++ src/Quiz/controller/quizController.ts
@@ -33,27 +33,5 @@ export class QuizController {
             io.to(quizRequest.roomCode).emit(hostEvents.Start_Quiz, _questionResponse as QuestionResponse);
  
         });
-    }
-}
-export const handleSubmitAnswer = (socket: Socket) => {
-    // socket.on('submitAnswer', (data: { questionId: string; answer: string }) => {
-    //     const result = quizService.submitAnswer(socket.id, data.questionId, data.answer);
-    //     socket.emit('answerResult', { correct: result.correct, score: result.score, feedback: result.correct ? 'Correto!' : 'Incorreto!' });
-
-    //     io.emit('updateLeaderboard', quizService.getCurrentQuizState().players);
-    // });
-};
-
-export const handleQuizStart = (socket: Socket) => {
-    socket.on(hostEvents.Start_Quiz, (quizRequest: StartQuizRequest) => {
-
-    })
-    // socket.on('startQuiz', () => {
-    //     const nextQuestion = quizService.startNewQuestion();
-    //     if (nextQuestion) {
-    //         io.emit('newQuestion', { questionId: nextQuestion.id, text: nextQuestion.text, options: nextQuestion.options });
-    //     } else {
-    //         socket.emit('error', { message: 'Nenhuma pergunta disponível ou quiz já iniciado.' });
-    //     }
-    // });
-};
+    } 
+} 
