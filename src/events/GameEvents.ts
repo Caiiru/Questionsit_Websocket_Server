cf5175af0c935d831f01fc18703f797502161326
Caiiru <54562856+Caiiru@users.noreply.github.com>
2025-08-11 17:04:22 -0300

    FEAT: Add Game Events

---
+++ src/events/GameEvents.ts
@@ -0,0 +1,12 @@
+export enum GameEvents{
+    StartQuiz = "START_QUIZ", 
+    SendTime = "SEND_TIME",
+    NextQuestion = "NEXT_QUESTION",
+    DisplayFeedback = "DISPLAY_FEEDBACK",
+    DisplayRanks = "DISPLAY_RANKS",
+    EndQuiz = "END_QUIZ",
+     
+    TimeEnded = "TIME_ENDED", //Termina o tempo do Quiz
+    AllPlayerAnswered = "PLAYERS_ANSWERED",
+    
+}
