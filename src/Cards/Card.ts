ee5687b8a961e4fc95dcc1dd69736cbbfb418259
Caiiru <54562856+Caiiru@users.noreply.github.com>
2025-08-20 13:32:54 -0300

    FEAT-WIP: Card&Card Service & Card Loader From JSON

---
+++ src/Cards/Card.ts
@@ -0,0 +1,14 @@
+export interface Card{
+    cardID:number,
+    cardName:string,
+    cardPW:number,
+    cardType:CardType //0 - instant, 1 - delayed
+    cardChance:number // 0-low, 1 - medium, 2- high chance
+}
+
+
+
+export enum CardType{
+    INSTANT = 0,
+    DELAYED = 1,
+}
